690876000a687de12d9e9df3a8c6dc38963e9aa0
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2018-12-12 14:36:41 +0300

    remove outdate test

--- jobfinder-server/src/test/java/ru/mityushin/jobfinder/server/JobFinderServerApplicationTests.java
+++ jobfinder-server/src/test/java/ru/mityushin/jobfinder/server/JobFinderServerApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.mityushin.researchwork.server;
+package ru.mityushin.jobfinder.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ResearchServerApplicationTests {
+public class JobFinderServerApplicationTests {
 
     @Test
     public void contextLoads() {
