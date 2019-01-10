25c428f09b6fb6fa2ce94bb4b2f73d0cb63d8f40
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2019-01-11 02:41:02 +0300

    add https

--- jobfinder-server/src/main/java/ru/mityushin/jobfinder/server/util/config/WebSecurityConfig.java
+++ jobfinder-server/src/main/java/ru/mityushin/jobfinder/server/util/config/WebSecurityConfig.java
@@ -15,6 +15,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
